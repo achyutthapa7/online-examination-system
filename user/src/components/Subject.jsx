20073c08a0ca00e2f17cf3d0551ac0a9b50b5628
sarthak-gc <sarthakgc.np@gmail.com>
2024-12-19 21:50:26 +0545

    some ui improvements

--- user/src/components/Subject.jsx
+++ user/src/components/Subject.jsx
@@ -9,7 +9,7 @@ const Subject = ({ subject }) => {
   console.log(subject, "subject");
 
   return (
-    <div className="flex flex-col items-start justify-between px-8 py-4 rounded-lg shadow-md b-white min-h-[160px] transition hover:shadow-lg">
+    <div className="flex flex-col items-start justify-between px-8 py-4 rounded-lg shadow-md bg-white min-h-[160px] transition hover:shadow-lg">
       <h2 className="mb-2 text-2xl font-semibold ">{subject.subject}</h2>
       {/* <p className="text-gray-600">
         <strong>Subject:</strong> {subject}
