45216fd9978641c69f148545f0c738f6c718ee50
sarthak-gc <sarthakgc.np@gmail.com>
2024-12-21 23:53:47 +0545

    Deleted unnecessary schema

--- backend/models/exam.model.js
+++ backend/models/exam.model.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
-const questionSchema = new mongoose.Schema({
-  questionText: { type: String, required: true },
-  options: { type: [String], required: true },
-  correctAnswer: { type: Number, required: true },
-});
 
 const examSchema = new mongoose.Schema(
   {
