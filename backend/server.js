d506f62a9cbe5abd9209c72d4af8217f90f02ce6
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-03-28 21:29:51 +0545

    added rollup options

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ connectDB();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:4173",
+    ],
   })
 );
 
