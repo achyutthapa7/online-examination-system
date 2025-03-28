1010148164cdeea06c7dcc337c420f28520bf9d8
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-03-28 21:46:09 +0545

    added vercel.json

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:4173",
+      "https://online-examination-system-seven-sable.vercel.app",
     ],
   })
 );
