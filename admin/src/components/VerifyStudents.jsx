de610390373e155531b4c7728f857d29d2fc0519
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-03-29 00:00:09 +0545

    d

--- admin/src/components/VerifyStudents.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const VerifyStudents = () => {
-  return (
-    <div>Verify Students</div>
-  )
-}
-
-export default VerifyStudents
--- backend/utils/handleError.js
+++
@@ -1,5 +0,0 @@
-const handleError = (res, error) => {
-  console.error(`The error is: ${error.message}`);
-  res.status(500).send({ "Server error": error.message });
-};
-module.exports = handleError;
