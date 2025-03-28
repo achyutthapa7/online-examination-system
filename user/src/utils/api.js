63e99fafd4ad63a3aa11468cb75dd8f42a0acec7
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-03-29 00:46:34 +0545

    af

--- user/src/utils/api.js
+++ user/src/utils/api.js
@@ -5,6 +5,7 @@ export const API_URL =
   import.meta.env.MODE === "production"
     ? import.meta.env.VITE_API_URL_PRODUCTION
     : import.meta.env.VITE_API_URL_DEVELOPMENT;
+console.log({ API_URL });
 export const registration = async (
   fullName,
   userName,
