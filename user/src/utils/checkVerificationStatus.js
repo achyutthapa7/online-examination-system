ccab3918545d0edccba2b57118a1d6359b0b83e0
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-03-30 12:21:58 +0545

    cors error

--- user/src/utils/checkVerificationStatus.js
+++ user/src/utils/checkVerificationStatus.js
@@ -1,13 +1,11 @@
 import { API_URL } from "./api";
 
 export const checkVerificationStatus = async (studentId) => {
-  return await fetch(
-    `${API_URL}/auth/getUserVerificationStatus/${studentId}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  return await fetch(`${API_URL}/auth/getUserVerificationStatus/${studentId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
 };
